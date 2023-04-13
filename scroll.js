bc9f158d5534a2def06ed8b39ac8b8559242d184
frankhurt <franciscoahurtado@gmail.com>
2023-04-13 01:54:14 -0500

    General format update

--- scroll.js
+++ scroll.js
@@ -1,16 +1,16 @@
+/*==================== SCROLL ROUTINE ====================*/
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            setTimeout(()=> {
-                entry.target.classList.add('show')
-            }
-            ,0);
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add("show");
+      }, 0);
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
--- splash.js
+++ splash.js
@@ -1,29 +1,27 @@
 /*==================== SPLASH SCREEN ====================*/
-let intro = document.querySelector('.intro');
-let logoHeader = document.querySelector('.logo__header');
-let logoSpan = document.querySelectorAll('.logoSplash');
+let intro = document.querySelector(".intro");
+let logoHeader = document.querySelector(".logo__header");
+let logoSpan = document.querySelectorAll(".logoSplash");
 
-window.addEventListener('DOMContentLoaded', () => {
-    
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (idx + 1) * 400);
+    });
+
     setTimeout(() => {
-
-        logoSpan.forEach((span, idx)=>{
-            setTimeout(() => {
-                span.classList.add('active');
-            }, (idx + 1) * 400)
-        });
-        
+      logoSpan.forEach((span, idx) => {
         setTimeout(() => {
-            logoSpan.forEach((span, idx)=>{
-                setTimeout(() => {
-                    span.classList.remove('active');
-                    span.classList.add('fade');
-                }, (idx + 1) * 50)
-            })    
-        },2000);
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (idx + 1) * 50);
+      });
+    }, 2000);
 
-        setTimeout(() => {
-            intro.style.top = '-100vh';
-        },2300);
-    })
-})
+    setTimeout(() => {
+      intro.style.top = "-100vh";
+    }, 2300);
+  });
+});
